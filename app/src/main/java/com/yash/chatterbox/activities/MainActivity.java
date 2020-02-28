04ced4633c93d3e687afb5ceaf0330c9378f83b6
himanshu8686 <him.sharma8686@gmail.com>
2020-02-28 10:28:22 +0530

    tab layout and view pager is set up

--- app/src/main/java/com/yash/chatterbox/activities/MainActivity.java
+++ app/src/main/java/com/yash/chatterbox/activities/MainActivity.java
@@ -3,15 +3,15 @@ package com.yash.chatterbox.activities;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
+import androidx.viewpager.widget.ViewPager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
-
 import com.bumptech.glide.Glide;
+import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -20,6 +20,9 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.yash.chatterbox.R;
+import com.yash.chatterbox.adapters.ViewPagerAdapter;
+import com.yash.chatterbox.fragments.ChatsFragment;
+import com.yash.chatterbox.fragments.UsersFragment;
 import com.yash.chatterbox.model.User;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -33,21 +36,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private String userId;
 
     private Toolbar toolbar_layout;
+    private TabLayout tab_layout;
+    private ViewPager view_pager;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         initializeViewsForMainActivity();
+
         setSupportActionBar(toolbar_layout);
         getSupportActionBar().setTitle("");
+        showUserOnToolbar();
 
+        setUpViewPager();
 
+    }
 
-         firebaseUser=FirebaseAuth.getInstance().getCurrentUser();
+    /**
+     * This setUpViewPager() method is user defined method for placing view pager adapter in view
+     */
+    private void setUpViewPager() {
+        ViewPagerAdapter viewPagerAdapter=new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragment(new ChatsFragment(),"Chats");
+        viewPagerAdapter.addFragment(new UsersFragment(),"Users");
+        view_pager.setAdapter(viewPagerAdapter);
+        tab_layout.setupWithViewPager(view_pager);
+    }
+
+    /**
+     * This showUserOnToolbar() method is user defined method and is used for customizing on the toolbar
+     */
+    private void showUserOnToolbar() {
+        firebaseUser= FirebaseAuth.getInstance().getCurrentUser();
         userId= firebaseUser.getUid();
-        databaseReference=FirebaseDatabase.getInstance().getReference("Users").child(userId);
+        databaseReference= FirebaseDatabase.getInstance().getReference("Users").child(userId);
 
         databaseReference.addValueEventListener(new ValueEventListener()
         {
@@ -74,12 +97,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
     }
 
+    /**
+     * All the views are going to be initialized in this method
+     */
     private void initializeViewsForMainActivity()
     {
         profile_image= findViewById(R.id.profile_image);
         tv_userName=findViewById(R.id.tv_userName);
 
         toolbar_layout=findViewById(R.id.toolbar_layout);
+        tab_layout=findViewById(R.id.tab_layout);
+        view_pager  = findViewById(R.id.view_pager);
     }
 
     @Override
